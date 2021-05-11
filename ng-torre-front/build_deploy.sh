3fae5591ed06a3a6b0d0c0d93d742ebc4894d167
root <root@instance-1.us-central1-a.c.torre-shoal.internal>
2021-05-11 04:52:36 +0000

    add nginx and change dockerfile

---
+++ ng-torre-front/build_deploy.sh
@@ -0,0 +1,9 @@
+export SERVICE_NAME_ENV=front
+export SERVICE_ROUTE_ENV=/opt/services/torreFront/ng-torre-front
+export BRANCH_NAME_ENV=master
+export GIT_FETCH_ENV=false
+export INTERN_PORT=81
+export PORT=80
+export SERVER_DEPLOYMENT=production
+./deploy.sh
+
